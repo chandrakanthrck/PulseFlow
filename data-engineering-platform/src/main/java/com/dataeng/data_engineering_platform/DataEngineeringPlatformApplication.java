2740f7a74baca383115be865b72767c105ae61e7
chandrakanthrck <chandrakanthr789@gmail.com>
2024-11-26 14:15:26 -0500

    Initial setup

---
+++ data-engineering-platform/src/main/java/com/dataeng/data_engineering_platform/DataEngineeringPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dataeng.data_engineering_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEngineeringPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataEngineeringPlatformApplication.class, args);
+	}
+
+}
---
+++ data-engineering-platform/src/test/java/com/dataeng/data_engineering_platform/DataEngineeringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataeng.data_engineering_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataEngineeringPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
